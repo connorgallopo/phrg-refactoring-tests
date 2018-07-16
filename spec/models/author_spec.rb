6344c1fea422f666e8ad490322a4af045457dc87
Connor Gallopo <connor.gallopo@me.com>
2018-07-16 09:44:20 -0400

    Added solution

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -2,23 +2,23 @@ require 'rails_helper'
 
 RSpec.describe Author, type: :model do
   it "is valid" do
-    author = Author.new(name: "Caligula", phone_number: 5553054425)
+    author = build(:author)
     expect(author).to be_valid
   end
 
   it "is invalid with no name" do
-    author = Author.new(phone_number: 5553054425)
+    author = build(:author, name: "")
     expect(author).to be_invalid
   end
 
   it "is invalid with a short number" do
-    author = Author.new(name: "Caligula", phone_number: 555305442)
+    author = build(:author, name: "Caligula", phone_number: 555305442)
     expect(author).to be_invalid
   end
 
   it "is invalid when non-unique" do
-    Author.create(name: "Caligula", phone_number: 5553054425)
-    author = Author.new(name: "Caligula", phone_number: 5557890001)
+    create(:author, name: "Caligula", phone_number: 5557890001)
+    author = build(:author, name: "Caligula", phone_number: 5557890001)
     expect(author).to be_invalid
   end
 
